d5b5693a4484360523abb7d2a411b39fba507b08
alina-sozoeva <sozoeva.alina@gmail.com>
2025-08-29 15:46:46 +0600

    add home page

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,10 +1,21 @@
+import { MenuOutlined } from "@ant-design/icons";
+import { Flex } from "antd";
+
+import logo from "../../assets/iprescribe-logo.svg";
+
 import styles from "./Header.module.scss";
 import clsx from "clsx";
 
 export const Header = () => {
   return (
-    <header>
-      <section className={clsx("container")}></section>
+    <header className={clsx(styles.header)}>
+      <section className={clsx("container")}>
+        <Flex className={clsx(clsx(styles.header_content))}>
+          <MenuOutlined className={clsx(styles.header_burger)} />
+          <span>LOGO</span>
+          {/* <img className={clsx(styles.header_img)} src={logo} alt={logo} /> */}
+        </Flex>
+      </section>
     </header>
   );
 };
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,69 @@
+import { useState } from "react";
+
+import { Flex } from "antd";
+import { RiFileEditLine } from "react-icons/ri";
+
+import rx from "../../assets/rx.png";
+
 import styles from "./HomePage.module.scss";
 import clsx from "clsx";
+import { BsStopwatch } from "react-icons/bs";
+import { FaArrowRotateRight } from "react-icons/fa6";
+import { HiOutlineChatAlt2 } from "react-icons/hi";
+import { GiMedicines } from "react-icons/gi";
+
+const acts = [
+  {
+    key: 1,
+    icon: <BsStopwatch />,
+    title: "Ожидание приема",
+  },
+  {
+    key: 2,
+    icon: <FaArrowRotateRight />,
+    title: "Продление",
+  },
+  {
+    key: 3,
+    icon: <HiOutlineChatAlt2 />,
+    title: "Безопасный чат",
+  },
+  {
+    key: 4,
+    icon: <GiMedicines />,
+    title: "ПДМП",
+  },
+];
 
 export const HomePage = () => {
   return (
     <main>
-      <section className={clsx("container")}>HomePage</section>
+      <div className={clsx(styles.main, "container h_header")}>
+        <Flex
+          vertical
+          justify="center"
+          align="center"
+          className={clsx(styles.new_rx)}
+        >
+          <img src={rx} alt={rx} />
+          <span>Новый рецепт</span>
+        </Flex>
+      </div>
+      <div className={clsx(styles.actions, "container")}>
+        {acts.map((item) => (
+          <Flex
+            vertical
+            key={item.key}
+            align="center"
+            justify="center"
+            gap="middle"
+            className={clsx(styles.act)}
+          >
+            {item.icon}
+            <span>{item.title}</span>
+          </Flex>
+        ))}
+      </div>
     </main>
   );
 };
